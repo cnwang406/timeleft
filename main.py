b6bfb05b2b0e178f02c34af2a4a67ef8b9724a7d
cnwang <cnwang406@gmail.com>
2021-07-25 21:55:35 +0800

    add header

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from common import MY_LOCATION
 from getservice import getService
 if (__name__ == "__main__"):
 
-    print(f'start')
-
+    print(f'===Time Left===')
+    print(f'by Lucas')
     getService()
     print("finished")
